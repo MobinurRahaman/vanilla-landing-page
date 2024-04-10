cd96eb77b77df0cb7986dd055467583bdef57304
Md Mobinur Rahaman <mobinurrahaman@gmail.com>
2024-04-10 18:29:46 +0530

    add form, card overlay etc.

---
+++ js/main.js
@@ -0,0 +1,6 @@
+document.getElementById("myForm").addEventListener("submit", function (event) {
+  event.preventDefault();
+  const formData = new FormData(this);
+  const formValues = Object.fromEntries(formData.entries());
+  console.log("Form Data:", formValues);
+});
